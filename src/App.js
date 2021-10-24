bba3f60506382fc305ab4a475124e1eb5e97ad3d
gupta-shatakshi <guptashatakshi30@gmail.com>
2021-10-25 00:57:20 +0530

    styled nav & cards and chnaged img

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import "./App.css";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import NavComponent from "./components/NavComponent";
 import FirstSectionComponent from "./components/landingPageComponent/FirstSectionComponent";
+import icon from "./images/AasaanPremiumLogo.png";
 
 function App() {
+  document.title = "Aasaan Premium";
+  document.icon = {icon};
   return (
-    <Router>
-      <NavComponent />
-      <FirstSectionComponent />
-    </Router>
+      <Router>
+        <NavComponent />
+        <FirstSectionComponent />
+      </Router>
+    
   );
 }
 
--- src/components/landingPageComponent/FirstSectionComponent.jsx
+++ src/components/landingPageComponent/FirstSectionComponent.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import '../../styles/firstSection.css';
 
-
 export default function FirstSectionComponent() {
     return (
         <div className="firstSection-main">
             <div className="firstSection-main-one">
                 <div className="firstSection-main-one-first">
                     <h1>Let's find you <br />the Best Insurance</h1>
-                    <h5>50+ insurers with one <br />of the best prices</h5>
-                    <h5>Quick, easy &  <br /> hassle free</h5>
+                    <div>
+                        <h5>50+ insurers with one <br />of the best prices</h5>
+                        <h5>Quick, easy &  <br /> hassle free</h5>
+                    </div>
                     <i class="fab fa-500px"></i>
                 </div>
                 <div className="firstSection-main-one-second">
-                    <img src="https://static.pbcdn.in/cdn/images/home-v1/pb-promise-new.png" alt="photo" />
+                    {/* <img src="https://static.pbcdn.in/cdn/images/home-v1/pb-promise-new.png" alt="photo" /> */}
+                    <img src="https://zoozoosolutions.com/wp-content/uploads/2020/05/pngwave-7-4.png" alt="image"/>
                     <h2>A commitment <br /> to our customers</h2>
                 </div>
             </div>
@@ -21,23 +23,31 @@ export default function FirstSectionComponent() {
                 <div className="cardsOne">
                     <div className="cards">
                         <h2 className="words">
-                        Let's find you the Best Insurance
+                        Business Insurance
                         </h2>
                     </div>
                     <div className="cards">
                         <h2 className="words">
-                        Let's find you the Best Insurance
+                        Individual Insurance
                         </h2>
                     </div>
                     <div className="cards">
                         <h2 className="words">
-                        Let's find you the Best Insurance
+                        Property Insurance
                         </h2>
                     </div>
                 </div>
                 <div className="cardsTwo">
-                <div className="cards"></div>
-                <div className="cards"></div>
+                    <div className="cards">
+                            <h2 className="words">
+                            Auto Insurance
+                            </h2>
+                        </div>
+                        <div className="cards">
+                            <h2 className="words">
+                            Life Insurance
+                            </h2>
+                        </div>
                 </div>
             </div>
         </div>
