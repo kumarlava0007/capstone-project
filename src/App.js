9f93929f22b4cea2e783da5b80acee9f6548b51f
Tarun Lahrod <lahrod.tar46@gmail.com>
2021-10-24 15:07:58 +0530

    fixed the box-shadow

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
-import NavComponent from './components/NavComponent';
-import FirstSectionComponent from './components/landingPageComponent/FirstSectionComponent';
+import "./App.css";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import NavComponent from "./components/NavComponent";
+import FirstSectionComponent from "./components/landingPageComponent/FirstSectionComponent";
 
 function App() {
   return (
     <Router>
-      < NavComponent />
-      < FirstSectionComponent />
+      <NavComponent />
+      <FirstSectionComponent />
     </Router>
   );
 }
