1960a707b7fa6ea593a4975f151225497e103ad3
kumarlava0007 <kumarlava0007@gmail.com>
2021-10-24 14:12:00 +0530

    landing page first commit

---
+++ src/components/landingPageComponent/firstSectionComponent.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import '../../styles/firstSection.css';
+
+
+export default function FirstSectionComponent() {
+    return (
+        <div className="firstSection-main">
+            <div className="firstSection-main-one">
+                <div className="firstSection-main-one-first">
+                    <h1>Let's find you <br />the Best Insurance</h1>
+                    <h5>50+ insurers with one <br />of the best prices</h5>
+                    <h5>Quick, easy &  <br /> hassle free</h5>
+                    <i class="fab fa-500px"></i>
+                </div>
+                <div className="firstSection-main-one-second">
+                    <img src="https://static.pbcdn.in/cdn/images/home-v1/pb-promise-new.png" alt="photo" />
+                    <h2>A commitment <br /> to our customers</h2>
+                </div>
+            </div>
+            <div className="firstSection-main-two">
+                <div className="cardsOne">
+                    <div className="cards">
+                        <h2 className="words">
+                        Let's find you the Best Insurance
+                        </h2>
+                    </div>
+                    <div className="cards">
+                        <h2 className="words">
+                        Let's find you the Best Insurance
+                        </h2>
+                    </div>
+                    <div className="cards">
+                        <h2 className="words">
+                        Let's find you the Best Insurance
+                        </h2>
+                    </div>
+                </div>
+                <div className="cardsTwo">
+                <div className="cards"></div>
+                <div className="cards"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
