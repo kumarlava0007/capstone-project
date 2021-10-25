9628ce960e4ff1b6b0d8e5d64e9ca9548b371942
kumarlava0007 <kumarlava0007@gmail.com>
2021-10-25 12:56:24 +0530

    final

--- src/components/landingPageComponent/FirstSectionComponent.jsx
+++ src/components/landingPageComponent/FirstSectionComponent.jsx
@@ -1,34 +1,18 @@
 import React from 'react';
 import '../../styles/firstSection.css';
 
-<<<<<<< HEAD
-=======
-
->>>>>>> main
 export default function FirstSectionComponent() {
     return (
         <div className="firstSection-main">
             <div className="firstSection-main-one">
                 <div className="firstSection-main-one-first">
                     <h1>Let's find you <br />the Best Insurance</h1>
-<<<<<<< HEAD
-                    <div>
-                        <h5>50+ insurers with one <br />of the best prices</h5>
-                        <h5>Quick, easy &  <br /> hassle free</h5>
-                    </div>
-                    <i class="fab fa-500px"></i>
-                </div>
-                <div className="firstSection-main-one-second">
-                    {/* <img src="https://static.pbcdn.in/cdn/images/home-v1/pb-promise-new.png" alt="photo" /> */}
-                    <img src="https://zoozoosolutions.com/wp-content/uploads/2020/05/pngwave-7-4.png" alt="image"/>
-=======
                     <h5>50+ insurers with one <br />of the best prices</h5>
                     <h5>Quick, easy &  <br /> hassle free</h5>
                     <i class="fab fa-500px"></i>
                 </div>
                 <div className="firstSection-main-one-second">
                     <img src="https://static.pbcdn.in/cdn/images/home-v1/pb-promise-new.png" alt="photo" />
->>>>>>> main
                     <h2>A commitment <br /> to our customers</h2>
                     <h2>random text</h2>
                     <h2>jgaihsuifhdhufhui</h2>
@@ -38,48 +22,23 @@ export default function FirstSectionComponent() {
                 <div className="cardsOne">
                     <div className="cards">
                         <h2 className="words">
-<<<<<<< HEAD
-                        Business Insurance
-=======
                         Let's find you the Best Insurance
->>>>>>> main
                         </h2>
                     </div>
                     <div className="cards">
                         <h2 className="words">
-<<<<<<< HEAD
-                        Individual Insurance
-=======
                         Let's find you the Best Insurance
->>>>>>> main
                         </h2>
                     </div>
                     <div className="cards">
                         <h2 className="words">
-<<<<<<< HEAD
-                        Property Insurance
-=======
                         Let's find you the Best Insurance
->>>>>>> main
                         </h2>
                     </div>
                 </div>
                 <div className="cardsTwo">
-<<<<<<< HEAD
-                    <div className="cards">
-                            <h2 className="words">
-                            Auto Insurance
-                            </h2>
-                        </div>
-                        <div className="cards">
-                            <h2 className="words">
-                            Life Insurance
-                            </h2>
-                        </div>
-=======
                 <div className="cards"></div>
                 <div className="cards"></div>
->>>>>>> main
                 </div>
             </div>
         </div>
